52e19ad64ffbe2f1955c313773e987be948f7408
Crowdbotics <engineering@crowdbotics.com>
2023-05-18 11:44:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-forest-dev-84340",
+  url: "https://hidden-forest-dev-84340.botics.co",
+  api: "https://hidden-forest-dev-84340.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
